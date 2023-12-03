5b8ba77f3dcbb880e05664d993aad6d7a11dde3f
Dror Atariah <drorata@gmail.com>
2023-12-03 08:55:35 +0100

    Initial version of flip calculator

---
+++ real_estate_tools/flip_calculator.py
@@ -0,0 +1,115 @@
+import streamlit as st
+from loguru import logger
+from real_estate_tools import flip_calculator_components as fcc
+import pandas as pd
+import plotly.express as px
+
+
+logger.info("Starting a flip calculation cycle")
+
+"""
+# Flip Calculator
+
+## General factors
+"""
+arv = st.number_input(
+    label="Provide estimated ARV ($)",
+    value=400000,
+    step=1000,
+    help="This will be used to derive the selling price.",
+)
+atractivity_factor = (
+    st.number_input(
+        label="Provide the attractivity factor (%)",
+        value=2,
+        min_value=0,
+        max_value=100,
+    )
+    / 100
+)
+estimated_holding_period = st.number_input(
+    label="Number of months expected for flip", min_value=1
+)
+
+
+selling_col, purchase_col = st.columns(2)
+
+selling_col.write("## Selling factors")
+purchase_col.write("## Purchase factors")
+
+selling_col.write(
+    ":blue[Provide estimations of the different factors to impact the **selling**"
+    " costs]"
+)
+purchase_col.write(
+    ":blue[Provide estimations of the different factors to impact the **purchase**"
+    " costs]"
+)
+
+selling_factors = fcc.sell_or_purchase_factors(selling_col, mode="sell")
+purchase_factors = fcc.sell_or_purchase_factors(purchase_col, mode="purchase")
+
+
+"""
+## Monthly holding costs
+
+Indicate expected monthly costs
+"""
+
+monthly_costs = pd.Series(
+    {
+        "taxes_per_month": st.number_input(label="Tax ($)", min_value=0.0, step=10.0),
+        "insurance_per_month": st.number_input(
+            label="Insurance ($)", min_value=0.0, step=10.0
+        ),
+        "utilities_gas_per_month": st.number_input(
+            label="Gas ($)", min_value=0.0, step=10.0
+        ),
+        "utilities_electricity_per_month": st.number_input(
+            label="Electricity ($)", min_value=0.0, step=10.0
+        ),
+        "utilities_water_sewer_per_month": st.number_input(
+            label="Water and sewer ($)", min_value=0.0, step=10.0
+        ),
+        "trash_per_month": st.number_input(label="Trash ($)", min_value=0.0, step=10.0),
+    },
+    name="monthly_costs",
+)
+
+holding_costs = pd.DataFrame(
+    {
+        "total": monthly_costs * estimated_holding_period,
+        "monthly": monthly_costs,
+    }
+)
+
+
+"""
+## Rehab costs
+
+Provide estimation of the rehab total costs
+"""
+
+total_rehab = st.number_input(label="Rehab costs ($)", min_value=0, step=500)
+
+
+"""## Summary"""
+
+result = pd.DataFrame({"purchase_price": range(1000, arv, 1000)})
+result["purchase_costs"] = result["purchase_price"].apply(
+    lambda x: purchase_factors.get_cost(x)
+)
+result["selling_costs"] = selling_factors.get_cost(arv)
+result["holding_costs"] = holding_costs["total"].sum()
+result["rehab_costs"] = total_rehab
+result["total_expenses"] = result.sum(axis=1)
+result["total_income"] = arv * (1 - atractivity_factor)
+result["ROI"] = 100 * (result["total_income"] / result["total_expenses"] - 1)
+# result = result.set_index("purchase_price")
+st.dataframe(result, height=150)
+
+
+fig = px.line(result, x="purchase_price", y="ROI")
+
+
+st.plotly_chart(fig)
---
+++ real_estate_tools/flip_calculator_components.py
@@ -0,0 +1,101 @@
+# import streamlit as st
+from typing import Union
+from pydantic import BaseModel
+import streamlit as st_import
+
+
+class SellPurchaseFactors(BaseModel):
+    broker_commission: Union[int, float]
+    fixed_broker_fee: Union[int, float]
+    closing_costs: Union[int, float]
+    transfer_tax: Union[int, float]
+    fixed_notary: Union[int, float]
+    deed_recording_fee: Union[int, float]
+    legal_fee: Union[int, float]
+
+    def get_cost(self, price: float) -> float:
+        return (
+            price * self.broker_commission
+            + self.fixed_broker_fee
+            + price * self.closing_costs
+            + price * self.transfer_tax
+            + self.fixed_notary
+            + self.deed_recording_fee
+            + self.legal_fee
+        )
+
+
+def sell_or_purchase_factors(st: st_import, mode: str) -> SellPurchaseFactors:
+    broker_commission = (
+        st.number_input(
+            label="Broker commission (%)",
+            value=6.0,
+            min_value=0.0,
+            max_value=100.0,
+            step=5.0,
+            key=f"{mode}_broker_commission",
+        )
+        / 100
+    )
+    fixed_broker_fee = st.number_input(
+        label="Fixed broker fee ($)",
+        value=500.0,
+        min_value=0.0,
+        step=10.0,
+        key=f"{mode}_fixed_broker_fee",
+    )
+    closing_costs = (
+        st.number_input(
+            label="Closing costs(%)",
+            value=0.0,
+            min_value=0.0,
+            max_value=100.0,
+            step=5.0,
+            key=f"{mode}_closing_costs",
+        )
+        / 100
+    )
+    transfer_tax = (
+        st.number_input(
+            label="Transfer tax (%)",
+            value=2.5,
+            min_value=0.0,
+            max_value=100.0,
+            step=5.0,
+            key=f"{mode}_transfer_tax",
+        )
+        / 100
+    )
+    fixed_notary = st.number_input(
+        label="Fixed notary costs($)",
+        value=0.0,
+        min_value=0.0,
+        step=10.0,
+        key=f"{mode}_notary",
+    )
+    deed_recording_fee = st.number_input(
+        label="Deed recording fee ($)",
+        value=0.0,
+        min_value=0.0,
+        step=10.0,
+        key=f"{mode}_deed_recording",
+    )
+    legal_fee = st.number_input(
+        label="Legal fees ($)",
+        value=0.0,
+        min_value=0.0,
+        step=10.0,
+        key=f"{mode}_legal_fees",
+    )
+
+    return SellPurchaseFactors(
+        **{
+            "broker_commission": broker_commission,
+            "fixed_broker_fee": fixed_broker_fee,
+            "closing_costs": closing_costs,
+            "transfer_tax": transfer_tax,
+            "fixed_notary": fixed_notary,
+            "deed_recording_fee": deed_recording_fee,
+            "legal_fee": legal_fee,
+        }
+    )
